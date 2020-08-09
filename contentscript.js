0cab3f89e4976dec8fa6ec748842c636423bc838
Wen-Shiu Hung <catinclay@google.com>
2020-08-09 11:16:16 -0700

    It just works.

---
+++ contentscript.js
@@ -0,0 +1,7 @@
+function inject(url) {
+    var s = document.createElement('script');
+    s.src = chrome.extension.getURL(url);
+    s.onload = function() { this.remove(); };
+    (document.head || document.documentElement).appendChild(s);
+}
+inject('injected.js');
